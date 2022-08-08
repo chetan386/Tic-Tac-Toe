55f9f9364dccbd3cd6f109a73e7d13053f830501
chetan <awasthichetan0@gmail.com>
2022-08-08 17:39:04 +0530

    added sass and set the styles

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
+
 const square = ({value}) => {
   return (
-        <button type="button">{value}</button>
+        <button type="button" className='square'>{value}</button>
   )
 }
 
