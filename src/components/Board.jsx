169d09147a67bca26cfb1c753420e76e1080779a
chetan <awasthichetan0@gmail.com>
2022-08-08 19:25:00 +0530

    added board state and react events

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,23 +1,68 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Square from './Square'
 
 const Board = () => {
+   
+   // First element is state and second element is update function.
+   const [board,setBoard] = useState( Array(9).fill(null));
+   // Now i want to update the state the state when i click on square now when events comes in.
+   // Go to Square.jsx to and event onClick and add callback function
+   // we need another state to keep track of another player
+    const [isNext,setIsNext] = useState(false);
+   //     return isNext? 'X': 'O' after this we should also update state other wise another player turn always remain false
+
+   const handleSquareClick = (position) => {
+      
+      if( board[position] ){
+         return;
+      }
+
+      setBoard( (prev) => {
+
+         return prev.map((square,pos) =>{
+            if(pos ===position ){
+               return isNext? 'X': 'O'
+            }
+
+            return square;
+         })
+      } )
+      setIsNext( (prev)=>!prev)
+   }
+  
+   const renderSquare = (position) => {
+        return(
+        <Square 
+         value= {board[position]}
+         onClick= {()=> handleSquareClick(position)} 
+         />
+        )
+   }
+
+
+
+
+
   return (
     <div className='board'>
      <div className='board-row'>
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
+        {/* <Square value={board[0]} onCLick{()=>{ */}
+         {/* // We need to pass 0 to know which square got clicked and this we have to do for each block and to reduce this create another function render */}
+         {/* handleSquareClick(0) */}
+      
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
      </div>
      <div className='board-row'>
-        <Square value={3} />
-        <Square value={4} />
-        <Square value={5} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
      </div>
      <div className='board-row'>
-        <Square value={6} />
-        <Square value={7} />
-        <Square value={8} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
      </div>
     </div>
   )
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-
-const square = ({value}) => {
+// Now we need to connect state over there and this event so that when i click button state got updated. 
+            // How we can get access of the state defined in parent component, Well we need to pass function that will do logic as a custom attribute to this square.
+const square = ({value,onClick}) => {
   return (
-        <button type="button" className='square'>{value}</button>
+        <button type="button" className='square' onClick={onClick}>
+          {value}  
+        </button>
   )
 }
 
